331b74fbbcc2a2c65943904cf755397f1f8bbedf
Patrick Grawehr <pgrawehr@hotmail.com>
2021-05-01 09:50:20 +0200

    Update example

--- examples/DueFlashStorageStructExample/DueFlashStorageStructExample.ino
+++ examples/DueFlashStorageStructExample/DueFlashStorageStructExample.ino
@@ -6,8 +6,8 @@ DueFlashStorage dueFlashStorage;
 
 // The struct of the configuration.
 struct Configuration {
-  uint8_t a;
-  uint8_t b;
+  uint32_t a;
+  uint32_t b;
   int32_t bigInteger;
   char* message;
   char c;
@@ -23,11 +23,47 @@ void setup() {
   /* Flash is erased every time new code is uploaded. Write the default configuration to flash if first time */
   // running for the first time?
   uint8_t codeRunningForTheFirstTime = dueFlashStorage.read(0); // flash bytes will be 255 at first run
-  Serial.print("codeRunningForTheFirstTime: ");
-  if (codeRunningForTheFirstTime) {
+  Serial.print("Flash start: 0x");
+
+  byte* startAddr = dueFlashStorage.readAddress(0);
+  Serial.println((unsigned int)startAddr, HEX);
+  int bytes = 0;
+  while (bytes < 512 * 1024)
+  {
+    int* data = (int*)(startAddr + bytes);
+    if (*data == 0xFFFFFFFF)
+     {
+     Serial.print("Free block at 0x");
+     Serial.println((unsigned int)data, HEX);
+     }
+     else
+     {
+      Serial.print("Used block at 0x");
+     Serial.println((unsigned int)data, HEX);
+     }
+    bytes+=512;
+  }
+
+  byte* firstDataAddress = dueFlashStorage.getFirstFreeBlock();
+  byte* flashStartAddress = dueFlashStorage.readAddress(0);
+
+  Serial.print("First free block is at: 0x");
+  Serial.println((uint32_t)firstDataAddress, HEX);
+  
+  delay(2000);
+  int* firstCall = (int*)firstDataAddress;
+  int startContent = *firstCall;
+  Serial.print("Found value at first data address: ");
+  Serial.println(startContent);
+  Serial.print("Address is : ");
+  Serial.println((uint32_t)firstDataAddress, HEX);
+  Serial.print("Is first start: ");
+  if (*firstCall == -1) 
+  {
     Serial.println("yes");
+    delay(1000);
     /* OK first time running, set defaults */
-    configuration.a = 1;
+    configuration.a = 22;
     configuration.b = 0;
     configuration.bigInteger = 1147483647; // my lucky number
     configuration.message = "Hello world!";
@@ -36,10 +72,12 @@ void setup() {
     // write configuration struct to flash at adress 4
     byte b2[sizeof(Configuration)]; // create byte array to store the struct
     memcpy(b2, &configuration, sizeof(Configuration)); // copy the struct to the byte array
-    dueFlashStorage.write(4, b2, sizeof(Configuration)); // write byte array to flash
+    Serial.print("Writing data to 0x");
+    Serial.println((uint32_t)firstDataAddress, HEX);
+    dueFlashStorage.write(firstDataAddress + 4, b2, sizeof(Configuration)); // write byte array to flash
 
     // write 0 to address 0 to indicate that it is not the first time running anymore
-    dueFlashStorage.write(0, 0); 
+    dueFlashStorage.write(firstDataAddress, 0, sizeof(uint32_t)); 
   }
   else {
     Serial.println("no");
@@ -47,52 +85,24 @@ void setup() {
 }
 
 void loop() {
-  /* read configuration struct from flash */
-  byte* b = dueFlashStorage.readAddress(4); // byte array which is read from flash at adress 4
-  Configuration configurationFromFlash; // create a temporary struct
-  memcpy(&configurationFromFlash, b, sizeof(Configuration)); // copy byte array to temporary struct
-
-  // print the content
+  byte* firstDataAddress = dueFlashStorage.getFirstFreeBlock();
+  Configuration* cfg = (Configuration*)(firstDataAddress + 4);
+    // print the content
   Serial.print("a:");
-  Serial.print(configurationFromFlash.a);
+  Serial.print(cfg->a);
 
   Serial.print(" b:");
-  Serial.print(configurationFromFlash.b);
+  Serial.print(cfg->b);
 
   Serial.print(" bigInteger:");
-  Serial.print(configurationFromFlash.bigInteger);
+  Serial.print(cfg->bigInteger);
 
   Serial.print(" message:");
-  Serial.print(configurationFromFlash.message);
+  Serial.print(cfg->message);
 
   Serial.print(" c:");
-  Serial.print(configurationFromFlash.c);
+  Serial.print(cfg->c);
   Serial.println();
-  Serial.println();
-
-  /* change some values in the struct and write them back */
-  // increment b by 1 (modulus 100 to start over at 0 when 100 is reached)
-  configurationFromFlash.b = (configurationFromFlash.b+1) % 100;
-
-  // change the message
-  String message = configurationFromFlash.message;
-  if (configurationFromFlash.message == "Hello world!")
-    configurationFromFlash.message = "Hello Arduino Due!";
-  else
-    configurationFromFlash.message = "Hello world!";
-
-  // write configuration struct to flash at adress 4
-  byte b2[sizeof(Configuration)]; // create byte array to store the struct
-  memcpy(b2, &configurationFromFlash, sizeof(Configuration)); // copy the struct to the byte array
-  dueFlashStorage.write(4, b2, sizeof(Configuration)); // write byte array to flash
 
   delay(1000);
 }
-
-
-
-
-
-
-
-
